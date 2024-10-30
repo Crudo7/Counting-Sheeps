be6ce10f68518153da5881c297e234b3ed897ae0
FranMunozGlez <franciscomgz11@gmail.com>
2024-10-30 14:46:25 +0100

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Counting-Sheeps
+
+# **COUNTING SHEEPS**
+
+
+This was my first project with Javascript and I enjoyed it.
+"True" represents sheeps, and "false" represents wolves. I had to use booleans to complete it.
+
+
---
+++ js/index.js
@@ -0,0 +1,17 @@
+function countSheeps(list){
+  let lengthList = list.length;
+  let sumSheeps = 0;
+
+  for (let index=0;index<lengthList;index++){
+     if();
+  }
+if()
+if()
+
+let list = [ true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true ]
+let result = countSheeps(list);
+console.log(result);
+
+list = [ false, false, false ]
+result = countSheeps(list);
+console.log(result);
