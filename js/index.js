ad3cd71a02212178c438afa2af212011a11a48b0
FranMunozGlez <franciscomgz11@gmail.com>
2024-10-30 15:19:56 +0100

    finalized project

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
 function countSheeps(list){
   let lengthList = list.length;
-  let sumSheeps = 0;
+  let sheeps = 0;
 
   for (let index=0;index<lengthList;index++){
-     if();
+      if (true==list[index]){
+          sheeps++;
+      };
   }
-if()
-if()
+
+  if(sheeps > 0){
+      return("There are "+sheeps+" sheep in total");
+  };
+
+  if(sheeps == 0){
+      return("UPS!!! Wolfs eaten Sheeps");
+  };
+
+}
 
 let list = [ true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true ]
 let result = countSheeps(list);
 console.log(result);
 
-list = [ false, false, false ]
+list = [ false, false, false ];
 result = countSheeps(list);
 console.log(result);
